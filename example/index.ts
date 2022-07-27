ffd33daf8ee794e5919d5039414734409c6fecdd
Seth Westphal <westy92@gmail.com>
2022-07-27 00:31:21 -0500

    Update example and metadata.

--- example/index.ts
+++ example/index.ts
@@ -3,7 +3,12 @@ import { HolidayApi } from 'holiday-api-js';
 const holidayApi = new HolidayApi({ apiKey: '<Your API Key Here>' });
 
 (async () => {
-  const result = await holidayApi.getEvents();
-  const randomEvent = result.events[Math.floor(Math.random() * result.events.length)];
-  console.log(`Today is ${randomEvent.name}! Find more information at: ${randomEvent.url}.`);
+  try {
+    const result = await holidayApi.getEvents();
+    const randomEvent = result.events[Math.floor(Math.random() * result.events.length)];
+    console.log(`Today is ${randomEvent.name}! Find more information at: ${randomEvent.url}.`);
+    console.log(`Rate limits remaining: ${result.rateLimit.remainingDay}/${result.rateLimit.limitDay} (day), ${result.rateLimit.remainingMonth}/${result.rateLimit.limitMonth} (month).`);
+  } catch (err) {
+    console.log(`There was an error: ` + err);
+  }
 })();
