838c2a95831c8e048dbcdac789437d4961c1cd65
Seth Westphal <westy92@gmail.com>
2022-12-04 16:26:02 -0600

    Bump version.

--- example/index.ts
+++ example/index.ts
@@ -7,7 +7,7 @@ const holidayApi = new HolidayApi({ apiKey: '<Your API Key Here>' });
     const result = await holidayApi.getEvents();
     const randomEvent = result.events[Math.floor(Math.random() * result.events.length)];
     console.log(`Today is ${randomEvent.name}! Find more information at: ${randomEvent.url}.`);
-    console.log(`Rate limits remaining: ${result.rateLimit.remainingDay}/${result.rateLimit.limitDay} (day), ${result.rateLimit.remainingMonth}/${result.rateLimit.limitMonth} (month).`);
+    console.log(`Rate limits remaining: ${result.rateLimit.remainingMonth}/${result.rateLimit.limitMonth} (month).`);
   } catch (err) {
     console.log(`There was an error: ` + err);
   }
